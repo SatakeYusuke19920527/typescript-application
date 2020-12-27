1cfb88ea0176dae0a18d59de8c1b46deb2174501
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-12-27 13:02:31 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,38 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [te, setTe] = useState<string>('');
+  const [te, setTe] = useState<string>('じゃんけんしましょう！');
+  const [cpuTe, setCpuTe] = useState<string>('CPU待機中');
   const gu = () => {
     setTe('ぐー');
+    cpu();
   };
   const tyoki = () => {
     setTe('ちょき');
+    cpu();
   };
   const pa = () => {
     setTe('ぱー');
+    cpu();
+  };
+  const cpu = () => {
+    let random = Math.floor(Math.random() * 3);
+    console.log(random, 'cpu');
+    if (random === 0) {
+      setCpuTe('ぐー');
+    } else if (random === 1) {
+      setCpuTe('ちょき');
+    } else {
+      setCpuTe('ぱー');
+    }
   };
   return (
     <div className="App">
       <button onClick={gu}>ぐー</button>
       <button onClick={tyoki}>ちょき</button>
       <button onClick={pa}>ぱー</button>
-      <h1>{te}</h1>
+      <h1>あなたの手：{te}</h1>
+      <h1>CPUの手：{cpuTe}</h1>
     </div>
   );
 }
